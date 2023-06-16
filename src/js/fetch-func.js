30d93a8d6afbdd51f1968d6213500727a983fed9
Marlen13 <olenamarinova@gmail.com>
2023-06-16 23:47:54 +0300

    categories list
    html, css, fetch, markup

--- src/js/fetch-func.js
+++ src/js/fetch-func.js
@@ -2,7 +2,17 @@
 // Потім їх можна буде використовувати при імпорті в нших файлах
 
 //REST API: books-backend.p.goit.global/books/category-list
-export function fetchAllCategories() {}
+const BASE_URL = 'https://books-backend.p.goit.global';
+export function fetchAllCategories() {
+    return fetch(`${BASE_URL}/books/category-list`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+      console.log(response);
+        return response.json();
+    })
+}
 
 //REST API: books-backend.p.goit.global/books/bookId
 export function fetchExactBook() {}
--- src/js/render-categories-list.js
+++ src/js/render-categories-list.js
@@ -1,3 +1,21 @@
 // Логіка, яка рендерить список категорій
 // Визиваеться одразу в index.js
 import { fetchAllCategories } from './fetch-func';
+const categoryList = document.querySelector('.category-list');
+const loadCategory = document.querySelector('.load');
+const afterOrg = document.querySelector('.after-org');
+const bookThumb = document.querySelector('.book-thumb');
+
+fetchAllCategories()
+  .then(data => {
+    console.log(data)
+    const markup = data.map(({ list_name }) => {
+      return `<li class="category-list-item global-list">${list_name}</li>`
+    })
+      .join('');
+    categoryList.insertAdjacentHTML('beforeend', markup);
+    loadCategory.classList.add('unvisible');
+    
+  }).catch(error => {
+    console.log(error)
+  })
