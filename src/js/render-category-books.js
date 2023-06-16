28147947968512d1ff32613220cbb8fe3945775a
millenialia <lenalebo@gmail.com>
2023-06-16 16:55:27 +0200

    updated top books

--- src/js/render-category-books.js
+++ src/js/render-category-books.js
@@ -1,28 +1,31 @@
 // Рендер однієї секції категорії з книгами
 import { fetchBooksByExactCategory, fetchTopBooks } from './fetch-func';
 
-export function renderCategoryBooks() {}
+// export function renderCategoryBooks() {}
 
-const containerEl = document.querySelector(".bestseller-container")
+const containerTbEl = document.querySelector(".tb-container")
 
 fetchTopBooks().then(renderTopBooks)
 
-
 export function renderTopBooks(bestsellersArray) {
-
+  console.log(bestsellersArray);
   const markup1 = bestsellersArray.map(({ list_name, books }) => {
     const markup = books.map(({ author, title, book_image }) => {
-      return `<div class="tb-book-card">
+      return `<a href=# class="global-link"><div class="tb-book-card">
       <img class="tb-book-img img" src="${book_image}" alt="${title}">
       <p class="tb-book-title">${title}</p>
       <p class="tb-book-author">${author}</p>
-      </div>`
+      </div></a>`
     }).join('')
-    return `<h2 class='tb-category'>${list_name}</h2>
+    return `<div class="tb-category-container">
+    <h2 class='tb-category'>${list_name}</h2>
     <div class='tb-books-container'>${markup}</div>
-    <button class="button">See more</button>`
+    <button class="tb-button global-button">See more</button>
+    </div>
+    `
   }).join('')
 
-containerEl.insertAdjacentHTML('beforeend', markup1)
+  containerTbEl.insertAdjacentHTML('beforeend', markup1)
 
 }
+
