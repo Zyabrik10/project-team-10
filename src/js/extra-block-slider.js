1f11e316f77b835236c520a7b7a54750186e1196
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-06-16 17:18:26 -0400

    support Ukraine added without svg icons

--- src/js/extra-block-slider.js
+++ src/js/extra-block-slider.js
@@ -1,1 +1,33 @@
 // Слайдер для Support Ukrain
+import { support_array } from './support-data';
+
+const supportEl = document.querySelector('ul.support-list');
+
+const supportUkraineMarkup = support_array
+  .map(({ title, url }, index) => {
+    return `<li class="support-item"><span class="item-number">${addNum(
+      index + 1
+    )}</span>
+     <a class="support-link" href=${url}>
+  ${title}
+</a>
+</li>
+    `;
+  })
+  .join('');
+
+{
+  /* <a class="support-link">
+  <svg class="support-svg">
+    <use
+      class="icon"
+      href=${img}
+    ></use>
+  </svg>
+</a>; */
+}
+supportEl.insertAdjacentHTML('beforeend', supportUkraineMarkup);
+
+function addNum(value) {
+  return value.toString().padStart(2, '0');
+}
