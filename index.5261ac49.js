23d26642cab9deeae45bc9bd3ec687e7f4143e8a
Zyabrik10 <78109707+Zyabrik10@users.noreply.github.com>
2023-06-16 19:01:16 +0000

    Deploying to gh-pages from @ Zyabrik10/project-team-10@88b5176660d1efd4b784a0a57542e7661539bd43 🚀

--- index.5261ac49.js
+++
@@ -1,2 +0,0 @@
-JSON.parse(localStorage.getItem("client-info"));
-//# sourceMappingURL=index.5261ac49.js.map
--- index.cd6fe3fc.js
+++
@@ -1,2 +0,0 @@
-JSON.parse(localStorage.getItem("client-info"));
-//# sourceMappingURL=index.cd6fe3fc.js.map
