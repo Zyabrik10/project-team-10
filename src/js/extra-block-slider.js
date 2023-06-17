3fdb1f5d649df9342aa76426f7f4ddc4db3bdd5f
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-06-17 15:27:58 -0400

    5 more images added

--- src/js/extra-block-slider.js
+++ src/js/extra-block-slider.js
@@ -4,12 +4,12 @@ import { support_array } from './support-data';
 const supportEl = document.querySelector('ul.support-list');
 
 const supportUkraineMarkup = support_array
-  .map(({ title, url, img }, index) => {
+  .map(({ title, url, img, id }, index) => {
     return `<li class="support-item"><span class="item-number">${addNum(
       index + 1
     )}</span>
      <a class="support-link global-link" href=${url} target="_blank">
-  <img src=${img}  width="131"  alt="${title}">
+  <img src=${img} class="support-img-${id}"  alt="${title}">
 </a>
 </li>
     `;
