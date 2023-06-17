c7b68430175bb997aabe5307e2aa57d53e385c12
Zyabrik10 <78109707+Zyabrik10@users.noreply.github.com>
2023-06-17 16:32:00 +0000

    Deploying to gh-pages from @ Zyabrik10/project-team-10@34f33b92f1c1272a3706fab6491de9fbf94017ce 🚀

--- shopping-list.1743d475.js
+++
@@ -1,2 +0,0 @@
-JSON.parse(localStorage.getItem("client-info"));
-//# sourceMappingURL=shopping-list.1743d475.js.map
--- shopping-list.6c716c55.js
+++
@@ -1,2 +0,0 @@
-JSON.parse(localStorage.getItem("client-info"));
-//# sourceMappingURL=shopping-list.6c716c55.js.map
