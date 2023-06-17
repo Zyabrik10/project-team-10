822e9e39613d585747047e7a71dc76e05921a661
NYarovenko <NYarovenko.Full.Stack@gmail.com>
2023-06-17 17:38:52 +0300

    shoppinlList card

--- src/js/render-shopping-list.js
+++ src/js/render-shopping-list.js
@@ -9,3 +9,15 @@ if (!shopping_info.shopping_list) {
   console.log('нічього');
   plug.style.display = 'none';
 }
+
+import { fetchExactBook } from './fetch-func';
+renderShopListBoock();
+
+async function renderShopListBoock() {
+  try {
+    const { data } = await fetchExactBook('643282b1e85766588626a0dc');
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
