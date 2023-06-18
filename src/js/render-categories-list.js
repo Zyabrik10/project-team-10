00c9832e22206662cb99d3c2daec42cd66df3797
Alex Mazurok | Мазурок Олександр | Zyabrik10 <78109707+Zyabrik10@users.noreply.github.com>
2023-06-18 23:14:35 +0300

    Revert "Category"

--- src/js/render-categories-list.js
+++ src/js/render-categories-list.js
@@ -1,20 +1,3 @@
 // Логіка, яка рендерить список категорій
 // Визиваеться одразу в index.js
 import { fetchAllCategories } from './fetch-func';
-const categoryList = document.querySelector('.category-list');
-const loadCategory = document.querySelector('.load');
-
-
-fetchAllCategories()
-  .then(data => {
-    console.log(data)
-    const markup = data.map(({ list_name }) => {
-      return `<li class="category-list-item global-list light-theme theme-switch">${list_name}</li>`
-    })
-      .join('');
-    categoryList.insertAdjacentHTML('beforeend', markup);
-    loadCategory.classList.add('unvisible');
-    
-  }).catch(error => {
-    console.log(error)
-  })
