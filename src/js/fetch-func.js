1b15cca3e1c6309a80a45eb9b907a135ead4a259
Zyabrik10 <sasamazurok@gmail.com>
2023-06-18 23:32:27 +0300

    Добавив функцію

--- src/js/fetch-func.js
+++ src/js/fetch-func.js
@@ -5,18 +5,29 @@ import axios from 'axios';
 //REST API: books-backend.p.goit.global/books/category-list
 const BASE_URL = 'https://books-backend.p.goit.global';
 export function fetchAllCategories() {
-    return fetch(`${BASE_URL}/books/category-list`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-      console.log(response);
-        return response.json();
-    })
+  return fetch(`${BASE_URL}/books/category-list`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    console.log(response);
+    return response.json();
+  });
+}
+
+export async function fetchTopBooks() {
+  try {
+    return await axios.get('', {
+      baseURL: 'https://books-backend.p.goit.global/books/top-books',
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 //REST API: books-backend.p.goit.global/books/bookId
 export function fetchExactBook() {}
+
+export function fetchTopBooks() {}
 
 //REST API: https://books-backend.p.goit.global/books/category?category=$
 
@@ -31,5 +42,3 @@ export async function fetchBooksByExactCategory(item) {
 }
 
 //REST API: https://books-backend.p.goit.global/books/top-books
-
-
--- src/js/render-category-books.js
+++ src/js/render-category-books.js
@@ -5,7 +5,7 @@ import { fetchBooksByExactCategory, fetchTopBooks } from './fetch-func';
 
 const categoryList = document.querySelector('.category-list');
 const bookThumb = document.querySelector('.tb-container');
-const headingEl = document.querySelector('.heading-primary')
+const headingEl = document.querySelector('.heading-primary');
 
 categoryList.addEventListener('click', renderCategoryBooks);
 async function renderCategoryBooks(event) {
@@ -17,39 +17,38 @@ async function renderCategoryBooks(event) {
   console.log(data);
   createMarkupBook(data);
 }
+
 function createMarkupBook({ data }) {
   bookThumb.innerHTML = '';
-    const markup = data.map(({ author, title, book_image }) => {
+  const markup = data
+    .map(({ author, title, book_image }) => {
       return `
     <li class="book-item tb-books-container flex-container-item">
     <img class="book-img tb-book-img img" src="${book_image}" alt="${title}">
     <p class="book-title tb-book-author global-p">${title}</p>
     <p>${author}</p>
-    </li>`
-    
+    </li>`;
     })
-      .join('');
+    .join('');
   bookThumb.innerHTML = markup;
-  }
-  
+}
 
 // export function renderTopBooks() { }
 
 const containerTbEl = document.querySelector('.tb-container');
-const noBooksEl = document.querySelector('.no-books-card')
-
+const noBooksEl = document.querySelector('.no-books-card');
 
 fetchTopBooks().then(renderTopBooks);
 
 export function renderTopBooks(bestsellersArray) {
-    if (bestsellersArray.data.length == 0) {
-    noBooksEl.classList.remove("is-hidden")
-  } else{
-  const markupCategory = bestsellersArray.data
-    .map(({ list_name, books }) => {
-      const markup = books
-        .map(({ author, title, book_image, _id }) => {
-          return `
+  if (bestsellersArray.data.length == 0) {
+    noBooksEl.classList.remove('is-hidden');
+  } else {
+    const markupCategory = bestsellersArray.data
+      .map(({ list_name, books }) => {
+        const markup = books
+          .map(({ author, title, book_image, _id }) => {
+            return `
           <li class="flex-container-item">
             <a href=# class="global-link" data-id="${_id}">
               <div class="tb-book-card">
@@ -60,9 +59,9 @@ export function renderTopBooks(bestsellersArray) {
             </a>
           </li>
           `;
-        })
-        .join('');
-      return `
+          })
+          .join('');
+        return `
       <div class="tb-category-container">
         <h2 class='tb-category global-title'>${list_name}</h2>
         <div class='tb-books-container'>
@@ -73,9 +72,9 @@ export function renderTopBooks(bestsellersArray) {
         <button class="tb-button global-button light-theme theme-switch">See more</button>
       </div>
       `;
-    })
-    .join('');
+      })
+      .join('');
 
-    containerTbEl.insertAdjacentHTML('beforeend', markupCategory);}
+    containerTbEl.insertAdjacentHTML('beforeend', markupCategory);
   }
-
+}
