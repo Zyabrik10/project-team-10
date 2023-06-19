5096dacaf7c5ae1dc4d3da56dca60146f9ce1237
NYarovenko <NYarovenko.Full.Stack@gmail.com>
2023-06-19 10:29:06 +0300

    adaptive layout

--- src/js/render-shopping-list.js
+++ src/js/render-shopping-list.js
@@ -45,24 +45,21 @@ function markupBookCard(data) {
           <p class="global-p shop_card-author">${author}</p>
           <div class="shop_card-empty_place"></div>
           <a class="global-link shop_card-link" href=${amazon_link} target="_blank" rel="noopener noreferrer">
-            <img
+            <img class="shop_amazon"
               src=${require('../images/shoppin-list/icons/amazon.png')}
               alt="amazon"
-              height="11px"
             />
           </a>
           <a class="global-link shop_card-link" href=${apple_books_link} target="_blank" rel="noopener noreferrer">
             <img
-              src= ${require('../images/shoppin-list/icons/books_1.png')}
+              src= ${require('../images/shoppin-list/icons/book_1.png')}
               alt="books"
-              height="16px"
             />
           </a>
           <a class="global-link shop_card-link" href=${bookshop_link} target="_blank" rel="noopener noreferrer">
             <img
-              src= ${require('../images/shoppin-list/icons/books_2.png')}
+              src= ${require('../images/shoppin-list/icons/book_2.png')}
               alt="books"
-              height="16px"
             />
           </a>
         </div>
