9331d5d6967c19e2a569fdd703d8e510e73522d4
Zyabrik10 <sasamazurok@gmail.com>
2023-06-20 10:44:20 +0300

    fix unworking

--- src/js/render-categories-books.js
+++ src/js/render-categories-books.js
@@ -2,7 +2,7 @@
 // Для цього можна використовувати масив категорій і по циклу визивати renderCategoryBooks(category)
 
 import { fetchAllCategories } from './fetch-func';
-import { renderCategoryBooks, renderTopBooks } from './render-category-books';
+// import { renderCategoryBooks, renderTopBooks } from './render-category-books';
 
 // одразу рендеремо секцію топ
 // renderTopBooks();
