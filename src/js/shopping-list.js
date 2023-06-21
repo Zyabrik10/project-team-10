258a5ae3cbd65cbc0786915456d6c52ea9e1b543
NYarovenko <NYarovenko.Full.Stack@gmail.com>
2023-06-21 03:55:59 +0300

    edits

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -3,30 +3,24 @@ export const shopping_info = JSON.parse(
   localStorage.getItem('client-info')
 ) || {
   theme: 'light',
-  shopping_list: {
-    '643282b1e85766588626a0dc': {
-      book_image:
-        'https://storage.googleapis.com/du-prd/books/images/9781984826398.jpg',
-      title: 'SWEET ENOUGH',
-      list_name: 'Advice How-To and Miscellaneous',
-      description: '',
-      author: 'Alison Roman',
-    },
-  },
+  shopping_list: {},
 };
 
-export function saveShoppingList() {
-  localStorage.setItem('client-info', JSON.stringify(shopping_info));
+export function saveShoppingList(listBooks) {
+  localStorage.setItem('client-info', JSON.stringify(listBooks));
 }
 
+// перезапис об'єкту після видалення однієї книжки
 export function removeShoppingListBoock(idBoock) {
   let result = {};
   for (const key in shopping_info.shopping_list) {
     if (key !== idBoock) result[key] = shopping_info.shopping_list[key];
   }
   localStorage.clear();
+  console.log('clear', shopping_info.shopping_list);
   saveShoppingList({
     theme: 'light',
     shopping_list: result,
   });
+  console.log('save', shopping_info.shopping_list);
 }
