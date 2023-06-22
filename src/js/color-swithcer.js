dc615a15f394e355a2334d58d47029477e70973b
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-06-22 00:14:59 -0400

    authorization added to the header

--- src/js/color-swithcer.js
+++ src/js/color-swithcer.js
@@ -1,3 +1,76 @@
+import { initializeApp } from 'firebase/app';
+import Notiflix from 'notiflix';
+
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDwpmuhImdY7RHC3U0YYZIshSlCJATjNaU',
+  authDomain: 'project-team-10-5792d.firebaseapp.com',
+  projectId: 'project-team-10-5792d',
+  storageBucket: 'project-team-10-5792d.appspot.com',
+  messagingSenderId: '76240638065',
+  appId: '1:76240638065:web:5518f239b638187e380bd0',
+  measurementId: 'G-CCNXH403RH',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+// const logOutEl = document.querySelector('.log-out');
+const loginSectionEl = document.querySelector('.login-section');
+
+const user = auth.currentUser;
+// const logInEl = document.querySelector('.login-section');
+const signUpEl = document.querySelector('#userProfileLoggedOut');
+const logOutEl = document.querySelector('#userProfileLoggedIn');
+
+const logOut = e => {
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+      Notiflix.Notify.success(`You are signing out...`);
+      setTimeout(
+        () => (window.location = 'http://localhost:1234/login.html'),
+        3000
+      );
+    })
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      Notiflix.Notify.failure(errorMessage);
+    });
+};
+
+const signUp = () => {
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+      Notiflix.Notify.info(`You are redirected to sign up...`);
+      setTimeout(
+        () => (window.location = 'http://localhost:1234/sign-up.html'),
+        3000
+      );
+    })
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      Notiflix.Notify.failure(errorMessage);
+    });
+};
+
+onAuthStateChanged(auth, user => {
+  if (user !== null) {
+    console.log('User logged in.');
+    signUpEl.classList.add('is-hidden');
+    logOutEl.addEventListener('click', logOut);
+  } else {
+    console.log('No user.');
+    logOutEl.style.display = 'none';
+    signUpEl.innerHTML = 'Sign Up';
+    signUpEl.addEventListener('click', signUp);
+  }
+});
+
 const switcher = document.querySelector('.switcher');
 
 switcher.addEventListener('click', function () {
@@ -14,7 +87,7 @@ switcher.addEventListener('click', function () {
 function toggleLogoImage() {
   const logoImages = document.querySelectorAll('.logo-image');
 
-  logoImages.forEach((logoImage) => {
+  logoImages.forEach(logoImage => {
     if (logoImage.classList.contains('light-theme')) {
       logoImage.src = require('/src/images/header/logo-light.png');
     } else if (logoImage.classList.contains('dark-theme')) {
@@ -23,25 +96,22 @@ function toggleLogoImage() {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   toggleLogoImage();
 
   const themeSwitch = document.querySelector('.theme-switch');
-  themeSwitch.addEventListener('click', function() {
-
+  themeSwitch.addEventListener('click', function () {
     toggleLogoImage();
   });
 });
 
-
-
-    // user login BUTTON  ----> якшо цей код не потрібен просто видаліть його:)
-    // var userProfile = document.querySelector('.user-profile');
+// user login BUTTON  ----> якшо цей код не потрібен просто видаліть його:)
+// var userProfile = document.querySelector('.user-profile');
 // поміняйте'modal' на своє --- ↓
-//     var modal = document.getElementById('modal'); 
+//     var modal = document.getElementById('modal');
 
 // userProfile.addEventListener('click', function() {
-//   modal.style.display = 'block'; 
+//   modal.style.display = 'block';
 // });
 
 // window.addEventListener('click', function(event) {
@@ -50,17 +120,17 @@ document.addEventListener('DOMContentLoaded', function() {
 //   }
 // });
 
-  // JS для перевірки залогінений чи ні користувач
+// JS для перевірки залогінений чи ні користувач
 
 // const userProfileLoggedOut = document.getElementById('userProfileLoggedOut');
 // const userProfileLoggedIn = document.getElementById('userProfileLoggedIn');
 
 // const userLoggedIn = onAuthStateChanged (auth, user => {
-//   if (user !== null) {  
+//   if (user !== null) {
 //   console.log('User logged in.');
-//   LogInEl.innerHTML = 'Log Out'; 
+//   LogInEl.innerHTML = 'Log Out';
 //   LogInEl.addEventListener('click', logout);
-  
+
 //   } else {
 //   console.log('No user.');
 //   LogInEl.innerHTML = 'Sign Up';
@@ -70,15 +140,5 @@ document.addEventListener('DOMContentLoaded', function() {
 // if (userLoggedIn) {
 //   userProfileLoggedOut.style.display = 'none';
 // } else {
-//   userProfileLoggedIn.style.display = 'none'; 
+//   userProfileLoggedIn.style.display = 'none';
 // }
-
-
-
-
-
-
-
-
-
-
