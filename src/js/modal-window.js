0d398450b13ce19cfb9702d90898d5c730e9bfa2
millenialia <lenalebo@gmail.com>
2023-06-22 13:57:44 +0200

    fix some bugs

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -5,6 +5,7 @@ import { removeBook } from './books-action';
 import { saveShoppingList, shopping_info } from './shopping-list';
 import { addBook, removeBook } from './books-action';
 import { fetchBook } from './fetch-func';
+import { renderTheme } from './switch-theme';
 
 export function modalWindow() {
   const body = document.querySelector('body');
@@ -65,7 +66,7 @@ export function modalWindow() {
       buy_links,
     }) {
       backdropModal.innerHTML = `
-                <div class="wrap">
+                <div class="wrap light-theme theme-switch">
                     <div class="modal-content-wrap">
                     <img class="book-icon" src="${book_image}" alt="Book wrap" width="287" height="458"/>
                     <div class="about-book">
@@ -89,6 +90,9 @@ export function modalWindow() {
                     <button class="close-btn global-button" type="button">&times;</button>
                     <p class="congratulations global-p no-content">Сongratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.</p>
                 </div>`;
+      shopping_info.theme === 'light'
+      ? renderTheme('light')
+      : renderTheme('dark');
     }
 
     function funcBtn(id, book) {
