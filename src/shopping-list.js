c83d4672ea591742faa07695efeea155c3c3eb21
Zyabrik10 <sasamazurok@gmail.com>
2023-06-23 13:19:59 +0300

    reset

--- src/shopping-list.js
+++
@@ -1,7 +0,0 @@
-import './js/switch-theme';
-import './js/extra-block-slider';
-import './js/modal-window';
-import './js/burger-menu';
-import './js/shopping-list';
-import './js/render-shopping-list';
-import './js/color-swithcer';
